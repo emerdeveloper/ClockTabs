18162db2e7ed414892670940db3ded8960fcffec
Emerson Javid Gonzalez Morales <1388310@SOAM.TCS.com>
2017-08-28 14:23:46 -0500

    project initial provided by Xamarin University to practice tabs in Android

---
+++ Clock/MainActivity.cs
@@ -0,0 +1,15 @@
+﻿using Android.App;
+using Android.OS;
+
+namespace Clock
+{
+	[Activity(Label = "Clock", MainLauncher = true, Icon = "@mipmap/icon")]
+	public class MainActivity : Android.Support.V4.App.FragmentActivity
+	{
+		protected override void OnCreate(Bundle savedInstanceState)
+		{
+			base.OnCreate(savedInstanceState);
+			SetContentView(Resource.Layout.Main);
+		}
+	}
+}
